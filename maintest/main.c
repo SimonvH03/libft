95de949247a6061f622ffa88bbac78e2f79e0d48
simon <si.hoogstraaten86@gmail.com>
2023-10-11 18:24:27 +0200

    maintest add

--- maintest/main.c
+++ maintest/main.c
@@ -3,29 +3,46 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: svan-hoo <svan-hoo@student.42.fr>          +#+  +:+       +#+        */
+/*   By: simon <simon@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/10 18:05:46 by svan-hoo          #+#    #+#             */
-/*   Updated: 2023/10/10 19:34:06 by svan-hoo         ###   ########.fr       */
+/*   Updated: 2023/10/11 13:30:10 by simon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+
+//singlecharacter
 int	main(void)
 {
-	char	alpha;
-	char	digit;
-	char	ascii;
-
-	alpha = 'Z';
-	digit = '9';
-	ascii = '-';
-	printf("atoi:		%d	[%d]\n", ft_atoi("  -0042x"), atoi("  -0042x"));
-	printf("isalnum:	%d\n", ft_isalnum(ascii));
-	printf("isalpa:		%d\n", ft_isalpha(digit));
-	printf("isascii:	%d\n", ft_isascii(ascii));
-	printf("isdigit:	%d\n", ft_isdigit(digit));
-	printf("isprint:	%d\n", ft_isprint(ascii));
+	printf("isalnum:	%d\n", ft_isalnum('b'));
+	printf("isalpa:		%d\n", ft_isalpha('b'));
+	printf("isascii:	%d\n", ft_isascii('-'));
+	printf("isdigit:	%d\n", ft_isdigit('1'));
+	printf("isprint:	%d\n", ft_isprint(' '));
+	printf("tolower:	%d\n", ft_tolower('B'));
+	printf("toupper:	%d\n", ft_toupper('b'));
 	return (0);
 }
+
+//str-mem inspection
+int	main(void)
+{
+	ft_strchr();
+	ft_strlen();
+	ft_strncmp();
+	ft_strnstr();
+	ft_strrchr();
+	ft_memchr();
+	ft_memcmp();
+	
+}
+
+//str-mem modification
+int	main(void)
+{
+	ft_strlcat()
+}
+
+//	printf("atoi:		%d	[%d]\n", ft_atoi("  -0042x"), atoi("  -0042x"));
