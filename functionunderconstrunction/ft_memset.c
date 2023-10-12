c4e5a28ad8c18709aefe6522688795b1de41c88f
Simon <svan-hoo@student.codam>
2023-10-12 15:42:57 +0200

    finished memset

--- functionunderconstrunction/ft_memset.c
+++ functionunderconstrunction/ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: svan-hoo <svan-hoo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/12 15:28:56 by svan-hoo          #+#    #+#             */
-/*   Updated: 2023/10/12 15:29:32 by svan-hoo         ###   ########.fr       */
+/*   Updated: 2023/10/12 15:42:21 by svan-hoo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,15 @@
 
 void	*ft_memset(void *s, int c, size_t n)
 {
-	
+	size_t			i;
+	unsigned char	*str;
+
+	i = 0;
+	str = s;
+	while (n--)
+	{
+		str[i] = c;
+		i++;
+	}
+	return (s);
 }
