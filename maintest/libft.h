93b72161aea2a7bf3eed0250d9264d769bf8110a
Simon <svan-hoo@student.codam>
2023-10-12 16:12:05 +0200

    cleaned up dir, moved finished funcitons

--- maintest/libft.h
+++ maintest/libft.h
@@ -6,7 +6,7 @@
 /*   By: svan-hoo <svan-hoo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/03 16:53:50 by svan-hoo          #+#    #+#             */
-/*   Updated: 2023/10/10 19:34:02 by svan-hoo         ###   ########.fr       */
+/*   Updated: 2023/10/12 16:10:52 by svan-hoo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 # include <stdlib.h>
 
 int		ft_atoi(char *c);
+void	ft_bzero(void *s, size_t n);
 int		ft_isalnum(int c);
 int		ft_isalpha(int c);
 int		ft_isascii(int c);
@@ -24,10 +25,13 @@ int		ft_isdigit(int c);
 int		ft_isprint(int c);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
+void	*ft_memcpy(void *dest, void *src, size_t n);
+void	*ft_memmove(void *dest, void *src, size_t n);
+void	*ft_memset(void *s, int c, size_t n);
 char	*ft_strchr(char *str, int c);
 int		ft_strlcat(char *dst, char *src, int l);
 int		ft_strlcpy(char *dst, char *src, int l);
-int		ft_strlen(char *c);
+size_t	ft_strlen(char *c);
 int		ft_strncmp(char *a, char *b, int n);
 char	*ft_strnstr(char *haystack, char *needle, int n);
 char	*ft_strrchr(char *str, int c);
