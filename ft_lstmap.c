762c43e698ed07c51310c661dbee6beb253af8d6
Simon <svan-hoo@student.codam>
2023-10-30 17:06:12 +0100

    x

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: svan-hoo <svan-hoo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/26 17:21:57 by svan-hoo          #+#    #+#             */
-/*   Updated: 2023/10/30 16:48:30 by svan-hoo         ###   ########.fr       */
+/*   Updated: 2023/10/30 16:52:51 by svan-hoo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,8 +31,8 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 			ft_lstclear(&first, del);
 			return (NULL);
 		}
-		ft_lstadd_back(&first, newnode);
 		lst = lst->next;
+		ft_lstadd_back(&first, newnode);
 	}
 	return (first);
 }
