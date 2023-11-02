90c08542f2bb762a48b52d1b1b7aed0efa3eea46
Simon <svan-hoo@student.codam>
2023-11-02 21:02:34 +0100

    calloc more smol

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: svan-hoo <svan-hoo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/12 15:39:37 by svan-hoo          #+#    #+#             */
-/*   Updated: 2023/11/02 20:35:23 by svan-hoo         ###   ########.fr       */
+/*   Updated: 2023/11/02 21:00:38 by svan-hoo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,13 @@
 
 void	*ft_calloc(size_t nmemb, size_t size)
 {
-	size_t	total;
 	void	*ptr;
 
 	if (size != 0 && nmemb > INT_MAX / size)
 		return (NULL);
-	total = nmemb * size;
-	ptr = (void *)malloc(total);
+	ptr = (void *)malloc(nmemb * size);
 	if (ptr == NULL)
 		return (NULL);
-	ft_bzero(ptr, total);
+	ft_bzero(ptr, nmemb * size);
 	return (ptr);
 }
