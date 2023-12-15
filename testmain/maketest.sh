5df5a9d7f04afccbabc70e01303b8dd9bfb99e92
simon <si.hoogstraaten86@gmail.com>
2023-12-15 15:59:37 +0100

    program named test.a; bash removes test.a; compile error fixed

--- testmain/maketest.sh
+++ testmain/maketest.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 make -C .. -s all clean
 mv ../libft.a ./
-cc testmain.c libft.a
-./a.out
+cc testmain.c libft.a -o test.a
+./test.a
+rm test.a
--- testmain/testmain.c
+++ testmain/testmain.c
@@ -6,7 +6,7 @@
 /*   By: simon <simon@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/29 20:50:29 by svan-hoo          #+#    #+#             */
-/*   Updated: 2023/12/14 16:32:50 by simon            ###   ########.fr       */
+/*   Updated: 2023/12/15 15:55:31 by simon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,4 +25,3 @@ int	main(void)
 	write(1, "\n\n", 2);
 	return (0);
 }
-make -C .. -s all clean && mv ../libft.a ./ && cc testmain.c libft.a && ./a.out
