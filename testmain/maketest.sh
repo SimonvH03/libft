363968afb823815e91b8de4cad6b86a926558232
simon <si.hoogstraaten86@gmail.com>
2023-12-18 23:42:28 +0100

    moved maintest

--- testmain/maketest.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-make -C .. -s all clean
-cp ../libft.a ./
-cc testmain.c libft.a -o test.a
-./test.a
-rm test.a
--- testmain/valtest.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-make -C .. -s all clean
-cp ../libft.a ./
-cc testmain.c libft.a -o test.a -g
-valgrind ./test.a
-rm test.a
